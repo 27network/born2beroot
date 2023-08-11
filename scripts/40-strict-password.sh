32f05aecb42198038077b1e30528f53de453711b
xtrm <oss@xtrm.me>
2023-08-11 20:53:09 +0200

    ✨ feature(strict-password): password aging rules

--- scripts/40-strict-password.sh
+++ scripts/40-strict-password.sh
@@ -2,3 +2,10 @@
 
 # Install library
 apt install -y libpam-pwquality
+
+# Replace password aging
+chmod +w /etc/login.defs
+sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS	30/g' /etc/login.defs
+sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS	2/g' /etc/login.defs
+sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE	7/g' /etc/login.defs
+chmod -w /etc/login.defs
