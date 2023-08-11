0ca7e93774e4a471fa126cc13efb36f7460634d0
xtrm <oss@xtrm.me>
2023-08-12 01:55:27 +0200

    ✨ feature(setup-monitoring): remove useless files

--- scripts/60-setup-monitoring.sh
+++ scripts/60-setup-monitoring.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Move scripts
-ROOT_DIR="/root/b2br-scripts"
+ROOT_DIR="$HOME/b2br-scripts"
 mkdir -p $ROOT_DIR
 cp $HOME/b2br/monitoring.sh $ROOT_DIR/monitoring.sh
-cp $HOME/b2br/delay.sh $ROOT_DIR/delay.sh
 chmod 755 $HOME/b2br/monitoring.sh
-chmod 755 $HOME/b2br/delay.sh
 
 # Enable cron
 systemctl enable --now cron
 
 # Write crontab
 INTERVAL='*/10 * * * *'
-COMMAND="sh $ROOT_DIR/delay.sh; sh $ROOT_DIR/monitoring.sh"
+COMMAND="sh $ROOT_DIR/monitoring.sh"
 (crontab -l 2>/dev/null; echo "$INTERVAL $COMMAND") | crontab -
 
+# Ensure crontab loaded
+EDITOR="cat" crontab -e
