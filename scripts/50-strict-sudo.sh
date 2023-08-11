2466df035edbe3a66f7bd18ca57052b4046741e8
xtrm <oss@xtrm.me>
2023-08-11 16:21:33 +0200

    🐛 fix(strict-sudo): fix spelling

--- scripts/50-strict-sudo.sh
+++ scripts/50-strict-sudo.sh
@@ -12,7 +12,7 @@ function add_default() {
 
 sed -i 's/\:\/bin/\:\/bin\:\/snap\/bin/' /etc/sudoers
 sed -i 's/use_pty/requiretty/' /etc/sudoers
-add_default "passwd_tried=3"
+add_default "passwd_tries=3"
 add_default "badpass_message=\"can\'t even write a password lmao\""
 add_default "logfile=\"/var/log/sudo/sudo.log\""
 add_default "log_input, log_output"
