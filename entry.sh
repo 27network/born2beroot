13afc88d7f4fb64184b5155c5bcb7d309c6cb12c
xtrm <oss@xtrm.me>
2023-08-11 15:18:13 +0200

    ✨ feature(entry.sh): add update check + entry detection

--- entry.sh
+++ entry.sh
@@ -7,9 +7,26 @@ apt upgrade -y
 # Install base packages
 apt install -y build-essential curl wget neovim git
 
+# Check if we're already cloned
+REPO_URL=https://github.com/27network/born2beroot
+TARGET_DIR=$HOME/b2br
+
+if [ -d .git ]; then
+	if [ "$(git config --get remote.origin.url)" == "$REPO_URL" ]; then
+		echo "Already in a cloned repo, skipping clone..."
+		TARGET_DIR=$(pwd)
+	fi
+fi
+
 # Clone repository
-git clone https://github.com/27network/born2beroot $HOME/b2br
-cd $HOME/b2br
+if [ ! -d $TARGET_DIR ]; then
+  git clone $REPO_URL $TARGET_DIR
+fi
+
+# Check for updates?
+cd $TARGET_DIR
+git fetch
+git pull
 
 # Launch installer
 cd scripts
