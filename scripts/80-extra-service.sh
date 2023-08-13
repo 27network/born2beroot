f7c79b2b05f70cf23306fa72b6086190c3e88d76
xtrm <oss@xtrm.me>
2023-08-13 18:41:47 +0200

    🔨 chore(extra-service): try fixing ports

--- scripts/80-extra-service.sh
+++ scripts/80-extra-service.sh
@@ -10,8 +10,8 @@ systemctl enable --now vsftpd
 echo $1 | sudo tee -a /etc/vsftpd.userlist
 
 # Edit configuration
-sed -i 's/^listen=NO/listen=YES/g' /etc/vsftpd.conf
-sed -i 's/^listen_ipv6=YES/listen_ipv6=NO/g' /etc/vsftpd.conf
+sed -i 's/^listen=YES/listen=NO/g' /etc/vsftpd.conf
+sed -i 's/^listen_ipv6=NO/listen_ipv6=YES/g' /etc/vsftpd.conf
 sed -i 's/^anonymous_enable=YES/anonymous_enable=NO/g' /etc/vsftpd.conf
 sed -i 's/^#local_enable=YES/local_enable=YES/g' /etc/vsftpd.conf
 sed -i 's/^#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
@@ -21,8 +21,8 @@ echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
 echo "userlist_enable=YES" >> /etc/vsftpd.conf
 echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
 echo "userlist_deny=NO" >> /etc/vsftpd.conf
-pasv_min_port=1337
-pasv_max_port=1339
+pasv_min_port=1338
+pasv_max_port=1345
 echo "pasv_min_port=$pasv_min_port" >> /etc/vsftpd.conf
 echo "pasv_max_port=$pasv_max_port" >> /etc/vsftpd.conf
 
@@ -30,8 +30,7 @@ echo "pasv_max_port=$pasv_max_port" >> /etc/vsftpd.conf
 systemctl restart vsftpd
 
 # Allow ports
+ufw allow 20/tcp
 ufw allow 21/tcp
-for i in $(seq $pasv_min_port $pasv_max_port); do
-	ufw allow $i/tcp
-done
+ufw allow $passv_min_port:$passv_max_port/tcp
 systemctl restart ufw
