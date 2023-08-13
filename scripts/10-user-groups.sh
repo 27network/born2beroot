e570fa9770470b4d7bdc2e67c73253fe29fcd4b9
xtrm <oss@xtrm.me>
2023-08-13 20:58:44 +0200

    🐛 fix(user-groups): don't add the groups if they're already applied

--- scripts/10-user-groups.sh
+++ scripts/10-user-groups.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-groupadd user42
-groupadd sudo
-usermod -aG user42 $1
-usermod -aG sudo $1
+# Add the groups 
+groupadd -f user42
+groupadd -f sudo
+
+# Add the user to the groups if they're not already in them
+TARGET_USER=$1
+if ! groups $TARGET_USER | grep -q '\buser42\b'; then
+	usermod -aG user42 $TARGET_USER
+fi
+if ! groups $TARGET_USER | grep -q '\bsudo\b'; then
+	usermod -aG sudo $TARGET_USER
+fi
